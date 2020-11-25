0c556d51d82ae4db7c9f332c8d761f4d4a3af66f
Xavier Morin <xavier10@live.ca>
2020-11-24 20:58:14 -0500

    nothing

--- main.py
+++ main.py
@@ -10,21 +10,23 @@ def main():
     # Drop useless columns & rename label
     train = train.drop(['q2_label', 'q3_label', 'q4_label', 'q5_label', 'q6_label', 'q7_label'], axis=1)
     test = test.drop(['q2_label', 'q3_label', 'q4_label', 'q5_label', 'q6_label', 'q7_label'], axis=1)
-    train.rename(columns={'q1_label': 'label'})
-    test.rename(columns={'q1_label': 'label'})
+    # train.rename(columns={'q1_label': 'label'})
+    # test.rename(columns={'q1_label': 'label'})
 
     # Pre-process the data. Lower-case, strip https links, tokenize.
     train['tokenized'] = train['text'].apply(lambda x: (preprocess(x)))
 
     # Get corpus (all the words in vocabulary)
     corpus = get_corpus(train['tokenized'])
-
+    X_train = train['tokenized']
+    y_train = train['q1_label']
+  
     # Compute the Term Frequency Matrix use for training
-    X_train = compute_term_freq_matrix(train['tokenized'], corpus)
-    y_train = train['label']
+    freqs_dict={}
+    freqs_dictionnary(freqs_dict,X_train,y_train,corpus)
 
     #Initialize classifier and fit
-    nbc = NaivesBayesClassifier()
+    nbc = NaiveBayesClassifier(freqs_dict,smooth_factor)
     nbc.fit(X_train,y_train)
 
 def compute_term_freq_matrix(X,corpus):
@@ -33,7 +35,7 @@ def compute_term_freq_matrix(X,corpus):
     Element (i,j) is the frequency of word i in tweet j
     '''
     tfm = pd.DataFrame(index=corpus)
-    for x in X['tokenized']:
+    for x in X:
         tfm = pd.concat([tfm, compute_tfm_column(x,corpus)], axis=1)
     return tfm
 
@@ -43,6 +45,25 @@ def compute_tfm_column(tweet, corpus, smooth_factor=0.01):
     '''
     freqs=[(tweet.count(word)+smooth_factor)/(len(tweet)+(smooth_factor*len(corpus))) for word in corpus]
     return pd.DataFrame(freqs, index=corpus)
+
+def freqs_dictionnary(result,X_train,y_train,corpus):
+    for y, x in zip(y_train, X_train):
+        for word in x:
+            if word in corpus:
+                # define the key, which is the word and label tuple
+                pair = (word,y)
+
+                # if the key exists in the dictionary, increment the count
+                if pair in result:
+                    result[pair] += 1
+
+                # else, if the key is new, add it to the dictionary and set the count to 1
+                else:
+                    result[pair] = 1
+    return result
+
+
+
 
 def preprocess(text):
     '''
